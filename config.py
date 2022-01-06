1172113aa8e70f0b43d93ed64b21264b35578a2e
Ishwar Jagdale <ishwarrules@gmail.com>
2022-01-06 23:34:29 +0530

    change sslmode to allow

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 SQLALCHEMY_DATABASE_URI = environ["SQLALCHEMY_DATABASE_URI"]
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ENGINE_OPTIONS = {
-    "sslmode": "require"
+    "sslmode": "allow"
 }
 SECRET_KEY = environ["SECRET_KEY"]
 FRONTEND_SERVER = environ["FRONTEND_SERVER"]
