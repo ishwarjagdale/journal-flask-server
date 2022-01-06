657dbcd84afa8469193c027a25773bbfd61e29f4
Ishwar Jagdale <ishwarrules@gmail.com>
2022-01-07 00:24:14 +0530

    removed ssl trying to fix

--- config.py
+++ config.py
@@ -2,8 +2,5 @@ from os import environ
 # SQLALCHEMY_DATABASE_URI = "mysql://reactBlog:Ishwar#01@localhost/journal"
 SQLALCHEMY_DATABASE_URI = environ["SQLALCHEMY_DATABASE_URI"]
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_ENGINE_OPTIONS = {
-    "sslmode": "allow"
-}
 SECRET_KEY = environ["SECRET_KEY"]
 FRONTEND_SERVER = environ["FRONTEND_SERVER"]
