983b59ecd3943c4587d616a47aaf61bde1451883
Ishwar Jagdale <ishwarrules@gmail.com>
2022-01-08 10:06:55 +0530

    cors

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ from api.api import api
 from auth.auth import login_manager
 from flask_cors import CORS
 from auth.auth import toAuth
-
+from os import environ
 
 app = Flask(__name__)
+cors = CORS(origins=environ["FRONTEND_SERVER"], supports_credentials=True)
 
 with app.app_context():
     app.config.from_pyfile("config.py")
     app.register_blueprint(api)
     app.register_blueprint(toAuth)
     login_manager.init_app(app)
-    cors = CORS(origins=app.config["FRONTEND_SERVER"], supports_credentials=True)
     cors.init_app(app)
     db.init_app(app)
     db.create_all()
